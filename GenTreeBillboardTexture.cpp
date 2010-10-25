0b16e6dd29cac9fdc9adcbd135c9ed381dcfdb1e
Bc. Jan Kaláb <pitlicek@gmail.com>
2010-10-25 16:31:16 +0200

    Proper sizes data type

--- GenTreeBillboardTexture.cpp
+++ GenTreeBillboardTexture.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "GenTreeBillboardTexture.h"
 
-void GenTreeBillboardTexture(char *data, int width, int height, unsigned int seed) {
+void GenTreeBillboardTexture(char *data, size_t width, size_t height, unsigned int seed) {
 	TreeNode* tree = GenTreeBillboardTexture_grammar("tree1", 40, seed);  // args: name, iteration, seed
  	tree->printTree();
 	
