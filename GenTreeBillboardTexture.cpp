966a75e8b7a1eb42a635cc9e9b68947698aa5ff1
xkubik15 <xkubik15@stud.fit.vutbr.cz>
2010-10-25 15:49:10 +0200

    fix

--- GenTreeBillboardTexture.cpp
+++ GenTreeBillboardTexture.cpp
@@ -4,9 +4,9 @@
 
 void GenTreeBillboardTexture(/*char *data, int width, int height, float param*/) {
 	//time_t cas;
-	int seed = 23505; // time(&cas);
-  TreeNode* tree = GenTreeBillboardTexture_grammar("tree1",40,seed);  // args : name, , seed
-	tree->printTree();
+  int seed = 23505; // time(&cas);
+  TreeNode* tree = GenTreeBillboardTexture_grammar("tree1",40,seed);  // args : name, iteration , seed
+  tree->printTree();
 	
 	
 	GenTreeBillboardTexture_parametrize();
