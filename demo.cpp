252e09d05aba5107691e407cd301ab5f22ea126b
Bc. Jan Kaláb <pitlicek@gmail.com>
2010-11-12 16:13:32 +0100

    Bresenham na cary, opraven chaos v souradnicich.

--- demo.cpp
+++ demo.cpp
@@ -2,13 +2,13 @@
 #include "GenTreeBillboardTexture.h"
 
 int main() {
-	const size_t width = 10;
-	const size_t height = 10;
-	char asciiart[width][height];
+	const size_t width = 80;
+	const size_t height = 25;
+	char asciiart[height][width];
 	GenTreeBillboardTexture((char*)asciiart, width, height, 23505);
-	for (size_t x = 0; x < width; x++) {
-		for (size_t y = 0; y < height; y++) {
-			putchar(asciiart[x][y]);
+	for (size_t y = 0; y < height; y++) {
+		for (size_t x = 0; x < width; x++) {
+			putchar(asciiart[y][x]);
 		}
 		putchar('\n');
 	}
--- vis.cpp
+++ vis.cpp
@@ -1,18 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "vis.h"
 
+#define SWAP(a,b) a^=b^=a^=b;
+
 void putpixel(char* canvas, size_t width, size_t x, size_t y, char pixel) {
-	*(canvas + x * width + y) = pixel;
+	*(canvas + x + y * width) = pixel;
+}
+
+void drawline(char* canvas, size_t width, size_t x1, size_t y1, size_t x2, size_t y2, char pixel) {
+	bool steep = abs(y1 - y2) > abs(x1 - x2);
+	if (steep) {
+		SWAP(x1, y1);
+		SWAP(x2, y2);
+	}
+	if (x1 > x2) {
+		SWAP(x1, x2);
+		SWAP(y1, y2);
+	}
+	short step_y = 1;
+	if (y1 > y2) {
+		step_y = -1;
+	}
+	
+	size_t dx = abs(x2 - x1);
+	size_t dy = abs(y2 - y1);
+	ssize_t P = 2 * dy - dx;
+	size_t P1 = 2 * dy;
+	ssize_t P2 = P1 - 2 * dx;
+	size_t y = y1;
+	for (size_t x = x1; x <= x2; x++) {
+		if (steep) {
+			putpixel(canvas, width, y, x, pixel);
+		} else {
+			putpixel(canvas, width, x, y, pixel);
+		}
+		if (P >= 0) {
+			P += P2;
+			y += step_y;
+		}
+		else {
+			P += P1;
+		}
+	}
 }
 
 void GenTreeBillboardTexture_visualize(char* data, size_t width, size_t height) {
 	printf("Visualizing tree...\n");
 	memset(data, ' ', width * height);
-	for (size_t x = 0; x < width; x++) {
-		for (size_t y = 0; y < height; y++) {
-			putpixel(data, width, x, y, '0' + y + x);
-		}
-	}
+	drawline(data, width, 0, 0, width - 1, height - 1, '#');
+	drawline(data, width, width - 1, 0, 0, height - 1, '*');
+	drawline(data, width, 0, 0, width - 1, 0, '.');
+	drawline(data, width, width - 1, 0, width - 1, height - 1, '.');
+	drawline(data, width, width - 1, height - 1, 0, height - 1, '.');
+	drawline(data, width, 0, height - 1, 0, 0, '.');
 	return;
 }
