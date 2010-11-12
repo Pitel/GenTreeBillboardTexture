1ba5e626a6abd6aad060e3e0f5ce045786d06955
Bc. Jan Kaláb <pitlicek@gmail.com>
2010-11-12 15:11:27 +0100

    Vycisteni platna

--- vis.cpp
+++ vis.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "vis.h"
 
 void putpixel(char* canvas, size_t width, size_t x, size_t y, char pixel) {
@@ -7,6 +8,7 @@ void putpixel(char* canvas, size_t width, size_t x, size_t y, char pixel) {
 
 void GenTreeBillboardTexture_visualize(char* data, size_t width, size_t height) {
 	printf("Visualizing tree...\n");
+	memset(data, ' ', width * height);
 	for (size_t x = 0; x < width; x++) {
 		for (size_t y = 0; y < height; y++) {
 			putpixel(data, width, x, y, '0' + y + x);
