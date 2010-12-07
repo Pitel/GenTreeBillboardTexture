f9e7f32d0df39c33951acbe34d95fa2fbdf9fc33
funous <funous@seznam.cz>
2010-12-07 22:44:01 +0100

    prepnuto na smrk

--- GenTreeBillboardTexture.cpp
+++ GenTreeBillboardTexture.cpp
@@ -2,10 +2,10 @@
 #include "GenTreeBillboardTexture.h"
 
 void GenTreeBillboardTexture(SDL_Surface *data, size_t width, size_t height, unsigned int seed, unsigned int depth, SDL_Color wood, SDL_Color leafs) {
-	TreeNode* tree = GenTreeBillboardTexture_grammar("tree1", depth, seed);  // args: name, iteration, seed
+	TreeNode* tree = GenTreeBillboardTexture_grammar("picea", depth, seed);  // args: name, iteration, seed
 	//tree->printTree();
 	
-	GenTreeBillboardTexture_parametrize(tree, PTREE_1, seed);
+	GenTreeBillboardTexture_parametrize(tree, PTREE_PICEA, seed);
 	//tree->printTreeWithParams();
 	
 	GenTreeBillboardTexture_visualize(data, width, height, tree, wood, leafs);
