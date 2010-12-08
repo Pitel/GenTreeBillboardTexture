0e87af6f9aa4212598dba7b21772118910a2b24f
xkubik15 <xkubik15@stud.fit.vutbr.cz>
2010-12-08 15:01:26 +0100

    doxygen

--- grammar.h
+++ grammar.h
@@ -8,9 +8,8 @@
 #ifndef GENTREE_GRAMMAR_H
 #define GENTREE_GRAMMAR_H
 
-/** TreeNode 
- *
- *  Tøída reprezentující uzel stromu v grafové reprezentaci 
+/**
+ *  Trida reprezentujici uzel stromu v grafove reprezentaci 
  */ 
 class TreeNode
 {
@@ -25,32 +24,28 @@ class TreeNode
       vector<TreeNode*> childNodes;
 		  paramInfo param;
 
-    /** printTree()
-     *
-     *  Pomocna funkce pro ladeni vystupu
-     *  Funkce prida aktualni strom do fronty a spusti generovani vetvi
+    /**
+     *  Pomocna funkce pro ladeni vystupu.
+     *  Funkce prida aktualni strom do fronty a spusti generovani vetvi.
      *  (uspornejsi vypis)  
      */ 
     void printTree();
         
-    /** printTreeWithParams()
-     *
-     *  Pomocna funkce pro ladeni vystupu
-     *  Funkce prida aktualni strom do fronty a spusti generovani vetvi
+    /**
+     *  Pomocna funkce pro ladeni vystupu.
+     *  Funkce prida aktualni strom do fronty a spusti generovani vetvi.
      *  (rozsireny vypis)  
      */     
     void printTreeWithParams();
      
-    /** processPrint()
-     *
+    /**
      *  Volana funkci printTree(). Obstarava samotny vypis vsech
      *  uzlu rozgenerovaneho strom
      *  (usporny vypis)  
      */     
 		static void processPrint();
 		
-		/** processPrintWithParams()
-     *
+		/**
      *  Volana funkci printTreeWithParams(). Obstarava samotny vypis vsech
      *  uzlu rozgenerovaneho strom.
      *  (rozsireny vypis)  
@@ -59,8 +54,7 @@ class TreeNode
 };
 
 
-/** GenTreeBillboardTexture_grammar(PtreeType, int, int)
- *
+/**
  *  Vstupem funkce je typ stromu, uroven generovani (detail), a nahodne cislo
  *  Funkce vraci ukazatel na vygenerovany strom z uzlu TreeNode
  */ 
