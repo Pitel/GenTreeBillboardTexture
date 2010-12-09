9d8cb5299c65d7b544c6f1772fd30de7d072bda1
funous <funous@Javor.(none)>
2010-12-09 14:29:05 +0100

    konecne snad funkcni billboarding

--- billboard/billboard.cpp
+++ billboard/billboard.cpp
@@ -29,6 +29,9 @@ float sxmax = 0.0, symax = 0.0, szmax = 0.0;
 
 int depth = 40;
 int seed = 23505;
+
+int camposx = 0, camposy = 0; //pozice kamery
+
 bool isHoldKey = false;
 SDLKey holdKey = SDLK_ESCAPE;
 ////////////////////////////////////////////////////////////////////////////////
@@ -53,7 +56,7 @@ SDLKey holdKey = SDLK_ESCAPE;
 unsigned int width_tree = 600;
 unsigned int height_tree = 600;
 unsigned int seed_tree = 23505;
-unsigned int depth_tree = 1000;
+unsigned int depth_tree = 400;
 
 bool use_sdlsurface = false;
 
@@ -155,10 +158,10 @@ void onWindowRedraw()
 	if(isHoldKey == true){
 		switch(holdKey) {
 			case SDLK_ESCAPE : quit(); break;
-//			case SDLK_1 : depth = depth+1; newTree(depth, seed); break;
-//			case SDLK_2 : depth = max(0, depth-1); newTree(depth, seed); break;
-//			case SDLK_3 : depth = max(1, depth*2); newTree(depth, seed); break;
-//			case SDLK_4 : depth = depth/2; newTree(depth, seed); break;
+			case SDLK_a : camposx -= 1; break; //depth = depth+1; newTree(depth, seed); break;
+			case SDLK_d : camposx += 1; break; //depth = max(0, depth-1); newTree(depth, seed); break;
+			case SDLK_w : camposy -= 1; break; //max(1, depth*2); newTree(depth, seed); break;
+			case SDLK_s : camposy += 1; break; //depth = depth/2; newTree(depth, seed); break;
 //			case SDLK_5 : seed = rand(); newTree(depth, seed); break;
 						  //        case SDLK_3 : mode = 2; redraw(); break;
 						  //        case SDLK_4 : mode = 3; redraw(); break;
@@ -181,8 +184,14 @@ void onWindowRedraw()
 				glm::rotate(
 					glm::rotate(
 						glm::translate(
-							glm::mat4(1.0f),
-							glm::vec3(0, 0, -pz)
+                            glm::translate(
+                                glm::translate(
+                                    glm::mat4(1.0f),
+                                    glm::vec3(0, 0, -pz)
+                                    ),
+                                    glm::vec3(-camposx, 0, 0)
+                                ),
+                                glm::vec3(0, 0, -camposy)
 							),
                         rx, glm::vec3(0, 1, 0)
 						),
@@ -191,7 +200,11 @@ void onWindowRedraw()
 				glm::vec3(-(sxmin+sxmax)/2.0, -(symin+symax)/2.0, -(szmin+szmax)/2.0)
 				);
 
+
+    //mv = glm::translate(mv, glm::vec3(camposx, 0, 0));
+
 	glm::mat4 mvp = projection*mv;
+
 
     glLoadMatrixf(glm::value_ptr(mvp));
 
@@ -217,8 +230,14 @@ void onWindowRedraw()
 				glm::rotate(
 					glm::rotate(
 						glm::translate(
-							glm::mat4(10.0f),
-							glm::vec3(0, 0, -pz)
+                            glm::translate(
+                                glm::translate(
+                                    glm::mat4(1.0f),
+                                    glm::vec3(0, 0, -pz)
+                                    ),
+                                    glm::vec3(-camposx, 0, 0)
+                                ),
+                                glm::vec3(0, 0, -camposy)
 							),
                             rx, glm::vec3(0, 1, 0)
 						),
@@ -229,13 +248,112 @@ void onWindowRedraw()
 
 	mvp =  glm::rotate(projection*mv, -rx, glm::vec3(0, 1, 0)); //Pootoceni zpet
 
+   // mvp = glm::translate(mvp, glm::vec3(camposx, 0, 0));
+
     glLoadMatrixf(glm::value_ptr(mvp));
 
 
     glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, g_tree);
 
-	glBegin(GL_QUADS);
+//	glBegin(GL_QUADS);
+//		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
+//		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f, -1.0f, 0.0f);
+//		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, 1.0f, 0.0f);
+//		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, 1.0f, 0.0f);
+//	glEnd();
+
+
+	float x,y,z;
+	x = 1.4;
+	y = 0;
+	z = 4;
+
+
+	mv = glm::translate(
+            glm::translate(
+				glm::rotate(
+					glm::rotate(
+						glm::translate(
+                            glm::translate(
+                                glm::translate(
+                                    glm::mat4(1.0f),
+                                    glm::vec3(0, 0, -pz)
+                                    ),
+                                    glm::vec3(-camposx, 0, 0)
+                                ),
+                                glm::vec3(0, 0, -camposy)
+							),
+                            rx, glm::vec3(0, 1, 0)
+						),
+                        ry, glm::vec3(1, 0, 0)
+					),
+                    glm::vec3(-x,-y,-z)),
+				glm::vec3(-(sxmin+sxmax)/2.0, -(symin+symax)/2.0, -(szmin+szmax)/2.0)
+                );
+
+    //mvp = projection*mv;
+
+    glm::vec4 a =  mv * glm::vec4(0, 0, 0, 1);
+
+    glm::vec3 pos = glm::vec3(a[0]/a[3], a[1]/a[3], a[2]/a[3]);
+
+	float t = (atan(pos.x/(pos.z))/M_PI)*180;
+
+    cout << t << endl;
+	cout << "[" << pos.x << ", " << pos.y << ", " << pos.z << "]" << endl;
+
+//
+//
+//    mvp = glm::translate(mvp, glm::vec3(-pos.x, -pos.y, -pos.z));
+//    mvp = glm::rotate(mvp, t, glm::vec3(0, 1, 0));
+//    mvp = glm::translate( mvp, glm::vec3(pos.x, pos.y, pos.z));
+
+	mv = 
+		glm::rotate(
+		glm::translate(
+            glm::translate(
+				glm::rotate(
+					glm::rotate(
+						glm::translate(
+                            glm::translate(
+                                glm::translate(
+                                    glm::mat4(1.0f),
+                                    glm::vec3(0, 0, -pz)
+                                    ),
+                                    glm::vec3(-camposx, 0, 0)
+                                ),
+                                glm::vec3(0, 0, -camposy)
+							),
+                            rx, glm::vec3(0, 1, 0)
+						),
+                        ry, glm::vec3(1, 0, 0)
+					),
+                    glm::vec3(-x,-y,-z)),
+				glm::vec3(-(sxmin+sxmax)/2.0, -(symin+symax)/2.0, -(szmin+szmax)/2.0)
+                ),
+				t, glm::vec3(0, 1, 0));
+
+
+
+    mvp = projection*mv;
+
+
+
+
+//vypisy
+cout << pos.x << " " << pos.y << " " << pos.z << endl;
+//cout << a[0] << " " << a.x << " " << pos[2] << endl;
+//cout << camposx << " " << camposy << endl;
+
+	//mvp =  glm::rotate(projection*mv, -rx, glm::vec3(0, 1, 0)); //Pootoceni zpet
+
+
+   // mvp =  glm::translate(mvp, glm::vec3(x, y, z));
+
+    glLoadMatrixf(glm::value_ptr(mvp));
+
+    glBegin(GL_QUADS);
 		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
 		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f, -1.0f, 0.0f);
 		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, 1.0f, 0.0f);
@@ -326,7 +444,7 @@ int main(int argc, char *argv[])
         init(800, 600, 24, 24, 0);
 
         genTexture();
-        mainLoop();
+        mainLoop(100);
 
     } catch(exception & ex) {
         cout << "ERROR : " << ex.what() << endl;
@@ -335,3 +453,4 @@ int main(int argc, char *argv[])
 
     return EXIT_SUCCESS;
 }
+
