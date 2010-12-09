49765b9f4c7a365bdac1b2be302c6026c1c05125
Bc. Jan Kaláb <pitlicek@gmail.com>
2010-12-09 11:41:46 +0100

    Podrobnejsi dokumentace

--- GenTreeBillboardTexture.cpp
+++ GenTreeBillboardTexture.cpp
@@ -1,6 +1,15 @@
 #include <cstdio>
 #include "GenTreeBillboardTexture.h"
 
+/** Generování impostoru stromu.
+ * Vygeneruje texturu stromu.
+ * @param data Ukazatel na SDLSurface
+ * @param seed Seed pro generování náhodných čísel
+ * @param depth Hloubka L-systému
+ * @param typeTree Typ stromu
+ * @param wood Barva dřeva
+ * @param leafs Barva listů
+ */
 void GenTreeBillboardTexture(SDL_Surface *data, unsigned int seed, unsigned int depth, PTreeType typeTree, SDL_Color wood, SDL_Color leafs) {
 	TreeNode* tree = GenTreeBillboardTexture_grammar(typeTree, depth, seed);  // args: name, iteration, seed
 	//tree->printTree();
